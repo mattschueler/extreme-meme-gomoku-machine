2e50a6388054e4b7c50af047505b23b458c2452d
Matt <mjschueler@gmail.com>
2018-02-01 12:52:47 -0500

    adds move to move file when turn file found, then removes turn file

--- ai.c
+++ ai.c
@@ -32,13 +32,23 @@ int main(int argc, char** argv){
 	printf("%d",board[0][0]);
 	checkBoard();
 */
-	while(1) {
-		if (fopen("groupname.go", "r") != NULL) {
-			printf("turnfile was found\n");
-			break;
-		} else {
-			printf("turnfile was not found\n");
-			sleep(5);
-		}
+while(1) {
+	FILE *tf = fopen("meme-machine.go", "r");
+	if (tf != NULL) {
+		printf("turnfile was found\n");
+		FILE *mv = fopen("move_file","r+");
+		/*char opp_move_buf[256] = {0};
+		fgets(mv, opp_move_buf);*/
+		char move_buf[256] = {0};
+		sprintf(move_buf, "meme-machine %c %i", 'A', 12);
+		fputs(move_buf, mv);
+		fclose(mv);
+		remove("meme-machine.go");
+		fclose(tf);
+		sleep(0.25);
+	} else {
+		printf("turnfile was not found\n");
+		sleep(5);
 	}
+}
 }
