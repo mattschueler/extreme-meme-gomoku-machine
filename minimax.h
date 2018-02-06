28440e5a15dff706603415f0ac44faa5790bd2d6
Unknown <loganalexanderr@gmail.com>
2018-02-06 16:19:40 -0500

    curr player to char

--- minimax.h
+++ minimax.h
@@ -5,5 +5,5 @@
 
 int* dlminimax(struct Board b, char curr_player, int curr_depth);
 void getBestMoves(struct Board b, int **moves);
-int abEval(struct Board b, int** moves, int curr_player);
+int abEval(struct Board b, int** moves, char curr_player);
 Board haveChild(struct Board parent);
