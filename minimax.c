722905279b0d1f21a7d159751dcbba381bfc8753
Unknown <loganalexanderr@gmail.com>
2018-02-06 15:42:29 -0500

    Working toward a-b

--- minimax.c
+++ minimax.c
@@ -15,30 +15,25 @@ int* dlminimax(struct Board b, char curr_player, int curr_depth) {
 	}
 	/* Scan board for the best moves and move them into the move buffer. */
 	getBestMoves(b, moves);
+	/* Before expanding, see if we need to prune. */
+	
+
 	/* Repeat this process for the children of these children; to a predefined depth. */
 	if(curr_depth <= MAX_DEPTH) {
 		int m;
 		/* For each of these moves. */
-		for( m = 0; m < 10 ; m++) {
-			/* Creat a child on the board. */
-			Board child = haveChild(b);
-			/* Set the location of the move to a tile of the current player. */
-			child.board[moves[m][0]][moves[m][1]] = curr_player ? we_are : enemy_is;
-			/* Assign weights in the child board w/ new move added. */
-			assign_weights(child);
+		if(!dont){ /* If not pruned. */
+			for( m = 0; m < 10 ; m++) {
+				/* Creat a child on the board. */
+				Board child = haveChild(b);
+				/* Set the location of the move to a tile of the current player. */
+				child.board[moves[m][0]][moves[m][1]] = curr_player ? we_are : enemy_is;
+				/* Assign weights in the child board w/ new move added. */
+				assign_weights(child);
 
-			/* Now that we have our weights fully set up here, a-b pruning should go here. */
-			/* We'll need a check for whose turn it is, i.e. who is max and who is min. */
-			/* Max will be checking our Alpha, i.e. the largest number someone on the board can get. */
-			/* Min will look for the most valuable move on their turn. */
-				/* This is because our heuristic looks at both players and calculates the best move. */
-				/* So the best move for the opponent would be the worst move for us, Max. */
-			/* Alpha = best move on our turn. */
-			/* Beta = best move on their turn. */
-			/* If child Beta > child.child Alpha, don't expand the child. */
-
-			/* Recursively call this function on the child. */
-			moves[m] = dlminimax(child, !curr_player, curr_depth+1);
+				/* Recursively call this function on the child. */
+				moves[m] = dlminimax(child, !curr_player, curr_depth+1);
+			}
 		}
 	}
 	/* Return first thing in moves */
