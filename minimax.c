0bbb2915122c1bba5b91e62541c5216e9a12b7ce
Unknown <loganalexanderr@gmail.com>
2018-02-05 20:44:50 -0500

    added baby function and fixed definitions

--- minimax.c
+++ minimax.c
@@ -3,21 +3,22 @@
 
 int i = 0;
 
-void dlminimax(Board b){
+void dlminimax(struct Board b){
 	int whoturn = 1; /* 0 for us, 1 for the enemy. */
 	int m;
 	char** gamestate = b.board;
+	assign_weights(b); /* Now b.weights should be a list of all the weights. */
 	int moves[10][2] = getBestMoves(gamestate);
 	i++;
 	for(m = 0; m < 10 , i < MINIMAX_DEPTH; m++){
 		int tempmove[2] = moves[m];
-		Board child =
+		Board child = haveChild(b);
 
 		dlminimax();
 	}
 }
 
-int** getBestMoves(struct Board board){
+int** getBestMoves(struct Board b){
 	int moveset[10][3] = {-1,-1,-100000}; // x coord, y coord, score
 	for(int i=0; i<BOARD_SIZE; i++) {
 		for(int j=0; j<BOARD_SIZE; j++) {
@@ -36,4 +37,26 @@ int** getBestMoves(struct Board board){
 		}
 	}
 	return moveset;
+}
+
+Board haveChild(struct Board parent){
+	/* Initialize baby */
+	Board child;
+	/* Set parameters */
+	child.parent = parent;
+	child.children = NULL;
+	child.alpha = parent.alpha;
+	child.beta = parent.beta;
+	/* Copy board and weights. */
+	int i, j; /* Iterators. */
+	for(i = 0; i < BOARD_SIZE; i++){
+		for(j = 0; j < BOARD_SIZE; j++){
+			/* One-by-one copy. */
+			child.board[i][j] = parent.board[i][j];
+			child.weights[i][j] = parent.weights[i][j];
+		}
+	}
+	/* Set best move. */
+	board.bestmove = getBestMoves(child);
+	board.dont = 0; /* Starts out not pruned. */
 }
