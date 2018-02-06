88b8be7b6f6529f025fb0aa732e927343d1d0d43
Matt <mjschueler@gmail.com>
2018-02-05 20:41:04 -0500

    added method to get 10 best moves based on heuristic results

--- minimax.c
+++ minimax.c
@@ -11,13 +11,29 @@ void dlminimax(Board b){
 	i++;
 	for(m = 0; m < 10 , i < MINIMAX_DEPTH; m++){
 		int tempmove[2] = moves[m];
-		Board child = 
+		Board child =
 
 		dlminimax();
 	}
 }
 
-int** getBestMoves(char** board){
-	int moveset[10][2];
-
+int** getBestMoves(struct Board board){
+	int moveset[10][3] = {-1,-1,-100000}; // x coord, y coord, score
+	for(int i=0; i<BOARD_SIZE; i++) {
+		for(int j=0; j<BOARD_SIZE; j++) {
+			for(int k=0; k<10; k++) {
+				if(board.weights[i][j]>moveset[k][2]) {
+					for(int l=10; l>k; l--) {
+						moveset[l][0] = moveset[l-1][0];
+						moveset[l][1] = moveset[l-1][1];
+						moveset[l][2] = moveset[l-1][2];
+					}
+					moveset[k][0] = i;
+					moveset[k][1] = j;
+					moveset[k][2] = board.weights[i][j];
+				}
+			}
+		}
+	}
+	return moveset;
 }
